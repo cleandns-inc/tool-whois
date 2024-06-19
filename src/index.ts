d3003e4fc1716be64b963454f1ba34fe3e057b2d
Jeffrey Pinyan <jeffrey.pinyan@cleandns.email>
2024-06-19 15:24:51 -0400

    find registrar email

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,10 @@ const eventMap = new Map<string, WhoisTimestampFields>([
   ["expiration date", "expires"],
 ]);
 
-export async function whois(domain: string, options: WhoisOptions = { fetch: fetch }): Promise<WhoisResponse> {
+export async function whois(
+  domain: string,
+  options: WhoisOptions = { fetch: fetch }
+): Promise<WhoisResponse> {
   const fetch = options.fetch!;
 
   let url: string | null = null;
@@ -123,10 +126,56 @@ export async function whois(domain: string, options: WhoisOptions = { fetch: fet
                   (el: any[]) => el[3],
                   reg
                 );
+          const email =
+            [ent, ...(ent.entities || [])]
+              .filter((e) => e?.vcardArray)
+              .map((e) =>
+                findInObject(
+                  e.vcardArray,
+                  (el: any) => Array.isArray(el) && el[0] === "email",
+                  (el: any[]) => el[3],
+                  ""
+                )
+              )
+              .filter(Boolean)?.[0] || "";
+
           const events =
             ent.events || response.events || ent.enents || response.enents;
-          registrars.push({ id, name, events });
+          registrars.push({ id, name, email, events });
         }
+      }
+
+      if (
+        domain.endsWith(".is") &&
+        (ent.roles?.includes("technical") || ent.role === "technical")
+      ) {
+        const id = ent.handle;
+        const name =
+          parseInt(id) == id
+            ? (await ianaIdToRegistrar(parseInt(id)))?.name
+            : findInObject(
+                ent.vcardArray,
+                (el: any) =>
+                  Array.isArray(el) && (el[0] === "fn" || el[0] === "org"),
+                (el: any[]) => el[3],
+                id
+              );
+        const email =
+          [ent, ...(ent.entities || [])]
+            .filter((e) => e?.vcardArray)
+            .map((e) =>
+              findInObject(
+                e.vcardArray,
+                (el: any) => Array.isArray(el) && el[0] === "email",
+                (el: any[]) => el[3],
+                ""
+              )
+            )
+            .filter(Boolean)?.[0] || "";
+
+        const events =
+          ent.events || response.events || ent.enents || response.enents;
+        registrars.push({ id, name, email, events });
       }
 
       if (
@@ -156,12 +205,18 @@ export async function whois(domain: string, options: WhoisOptions = { fetch: fet
 
   // registrar
   const { events, ...registrar } = registrars.sort((a: any, b: any) => {
-    const aDate =
-      (a.events.find((ev: any) => ev.eventAction === "registration")
-        ?.eventDate || 0).toString().replace(/\+0000Z$/, 'Z');
-    const bDate =
-      (b.events.find((ev: any) => ev.eventAction === "registration")
-        ?.eventDate || 0).toString().replace(/\+0000Z$/, 'Z');
+    const aDate = (
+      a.events.find((ev: any) => ev.eventAction === "registration")
+        ?.eventDate || 0
+    )
+      .toString()
+      .replace(/\+0000Z$/, "Z");
+    const bDate = (
+      b.events.find((ev: any) => ev.eventAction === "registration")
+        ?.eventDate || 0
+    )
+      .toString()
+      .replace(/\+0000Z$/, "Z");
     return new Date(bDate).valueOf() - new Date(aDate).valueOf();
   })[0] || { id: 0, name: "" };
   response.registrar = registrar;
@@ -182,9 +237,10 @@ export async function whois(domain: string, options: WhoisOptions = { fetch: fet
   );
 
   // ts
-  response.ts = findTimestamps(
-    [ ...(thickResponse?.events || []), ...(thinResponse?.events || []) ]
-  );
+  response.ts = findTimestamps([
+    ...(thickResponse?.events || []),
+    ...(thinResponse?.events || []),
+  ]);
 
   return response;
 }
@@ -232,9 +288,11 @@ function findTimestamps(values: any[]) {
   }
 
   for (const [event, field] of eventMap) {
-    const date = events.find((ev: any) => ev?.eventAction?.toLocaleLowerCase() === event);
+    const date = events.find(
+      (ev: any) => ev?.eventAction?.toLocaleLowerCase() === event
+    );
     if (date?.eventDate) {
-      ts[field] = new Date(date.eventDate.toString().replace(/\+0000Z$/, 'Z'));
+      ts[field] = new Date(date.eventDate.toString().replace(/\+0000Z$/, "Z"));
     }
   }
 
